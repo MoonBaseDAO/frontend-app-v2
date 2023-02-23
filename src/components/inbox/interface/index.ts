abe06a4892cf46e6ac8f367c6da5748a09fe15b0
devofficer <myeyesontech@gmail.com>
2023-02-23 07:05:32 -0800

    [feat] task management system

---
+++ src/components/inbox/interface/index.ts
@@ -0,0 +1,5 @@
+export type ItemType = {
+  id: string;
+  prefix: string;
+  content: string;
+};
---
+++ src/pages/inbox/index.tsx
@@ -0,0 +1,12 @@
+import DragList from '@/components/inbox/drag-list';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const Inbox = () => {
+  return (
+    <>
+      <DragList />
+    </>
+  );
+}
+
+export default Inbox;
