3444d441540d067d64635f054eb8c6ea64e723fb
dreamofnikolay <happydev999@gmail.com>
2023-03-12 15:46:13 -0700

    [feat] messages spread on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Inter } from '@next/font/google';
-
-const inter = Inter({ subsets: ['latin'] })
+import useApi from '@/hooks/useApi';
 
 export default function Home() {
+  const url = "http://moonbase.info.gf:3001/v1/messages";
+  const { loading, data } = useApi(url);
+  if (loading) return <h1>Loading</h1>;
   return (
-    <>
-      Overview
-    </>
-  )
+    <div>
+      <h1>Data fetched successfully.</h1>
+      {JSON.stringify(data)}
+    </div>
+  );
 }
